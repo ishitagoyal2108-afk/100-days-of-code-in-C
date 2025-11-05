7a9ea456c887f0763df42991bf95b62dfdffd00f
ishitagoyal2108-afk <ishitagoyal2108@gmail.com>
2025-11-05 18:17:45 +0530

    Add code to insert an element into a list

---
+++ day34.c
@@ -0,0 +1,15 @@
+# Define a list
+my_list = [10, 20, 40, 50]
+
+# Element to insert
+element = 30
+
+# Position to insert (index 2)
+position = 2
+
+# Insert the element
+my_list.insert(position, element)
+
+# Result: [10, 20, 30, 40, 50]
+print(my_list)
+
